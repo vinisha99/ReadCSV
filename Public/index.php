6047a55623a3135ff6ecf1fd1d02937e7deb3097
vinisha <vinisha99@gmail.com>
2018-10-06 16:36:00 -0400

    Feature: Created start class and main method to read CSV file

--- Public/index.php
+++ Public/index.php
@@ -6,4 +6,62 @@
  * Time: 2:52 PM
  */
 
-echo 'hello';
+//echo "hello world";
+
+main::start("CSVFile.csv");
+
+ class main{
+
+
+    /**
+     * @param $filename
+     */
+    static public function start($fileName){
+
+        $openFile = fopen($fileName, "r");
+        //$fileArray[];
+
+
+
+        while(! feof($openFile))
+        {
+
+            /*$object = (object) $openFile;
+            foreach ($openFile as $key => $value)
+            {
+                $object->$key = $value;
+            }*/
+
+            print_r(fgetcsv($openFile));
+            /*echo "</br>";
+            print_r($object);*/
+
+        }
+        fclose($openFile);
+
+        /*foreach ($array as $key => $value)
+        {
+            $object->$key = $value;
+        }*/
+
+       /* $handle = fopen("test.csv", "r");
+        for ($i = 0; $row = fgetcsv($handle ); ++$i) {
+            // Do something will $row array
+        }
+        fclose($handle);*/
+
+
+        echo 'file printed';
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
