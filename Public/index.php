27ee6359d9e7bd0b1d2ce7700cd477074fa72959
vinisha <vinisha99@gmail.com>
2018-10-06 20:40:36 -0400

    Fix: Code Refracting => reading CSV file into objects

--- Public/index.php
+++ Public/index.php
@@ -18,64 +18,43 @@ main::start("CSVFile.csv");
      */
     static public function start($fileName){
 
-        $openFile = fopen($fileName, "r");
-        $newArray = array();
-        $i=0;
 
-
-
-        while(! feof($openFile))
-        {
-
-            $fileArray = fgetcsv($openFile);
-
-
-            $objectFile = (object) $fileArray;
-            /*foreach ($fileArray as $key => $value)
-            {
-                $objectFile->$key = $value;
-
-                $newArray[$i] = $objectFile;
-            }*/
-            $newArray[$i] = $objectFile;
-            $i++;
-
-
-
-            //$fileArray = fgetcsv($openFile);
-
-            //$fileRecords[] = fileActions::convertArrayToObject($fileArray);
-            //print_r($fileArray);
-            //echo "</br>";
-            //$newArray = $objectFile;
-            //print_r($objectFile);
-            //print_r($newArray);
-            //print_r($fileRecords);
-
-        }
-        fclose($openFile);
-        print_r($newArray);
-
-
-        /*foreach ($array as $key => $value)
-        {
-            $object->$key = $value;
-        }*/
-
-       /* $handle = fopen("test.csv", "r");
-        for ($i = 0; $row = fgetcsv($handle ); ++$i) {
-            // Do something will $row array
-        }
-        fclose($handle);*/
-
-
-        echo 'file printed';
-
-
+        print_r(FileActions::readFile($fileName));
 
     }
 
+}
 
+class FileActions{
+
+     static public function readFile($file)
+     {
+         $openFile = fopen($file, "r");
+         $fileObjectData = array();
+         $i = 0;
+
+         while(! feof($openFile))
+         {
+
+             $fileArray = fgetcsv($openFile);
+             $fileObject = self::convertToObject($fileArray);
+
+             $fileObjectData[$i] = $fileObject;
+             $i++;
+
+         }
+         fclose($openFile);
+         //print_r($fileObjectData);
+         return $fileObjectData;
+
+     }
+
+     static public function convertToObject($fileData)
+     {
+         $obFile = (object) $fileData;
+
+         return $obFile;
+     }
 }
 
 
